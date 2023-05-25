65efa04db72c5f79fd543c0705673707be648238
abaikb <abaikb@icloud.com>
2023-05-25 17:10:58 +0600

    add wok-menu component

--- src/Pages/Soup/soup.jsx
+++ src/Pages/Soup/soup.jsx
@@ -3,7 +3,7 @@ import SoupMenu  from "../../components/soup-menu/soup-menu"
 
 export const SoupPage = () => {
     return (
-        <div className="home_container">
+        <div>
             <SoupMenu/>
         </div>
     )
